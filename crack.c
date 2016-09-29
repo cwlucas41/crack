836fdbf8aa59b4f1316f976fe6649a31e6d6f7b2
Christopher Lucas <chris@chriswlucas.com>
2016-09-28 23:58:23 -0500

    added extra credit logic

--- crack.c
+++ crack.c
@@ -23,11 +23,11 @@ bool checker(const char* guess, const char* salt, const char* correct) {
 }
 
 void usage() {
-	printf("crack <threads> <keysize> <target>\n");
+	printf("crack <threads> <keysize> <target> [expanded]\n");
 	exit(-1);
 }
 
-char* crackHelper(char* guess, const int currIdx, const int length) {
+char* crackHelper(char* guess, const int currIdx, const int length, const char start, const char end) {
 	if (currIdx == length) {
 		if (checker(guess, salt, target)) {
 			return guess;
@@ -36,9 +36,9 @@ char* crackHelper(char* guess, const int currIdx, const int length) {
 		}
 	} 
 
-	for (char c = 'a'; c <= 'z'; c++) {
+	for (char c = start; c <= end; c++) {
 		guess[currIdx] = c;
-		char* result = crackHelper(guess, currIdx + 1, length);
+		char* result = crackHelper(guess, currIdx + 1, length, start, end);
 		if (result != NULL) {
 			return result;
 		}
@@ -49,23 +49,42 @@ char* crackHelper(char* guess, const int currIdx, const int length) {
 	return NULL;
 }
 
-char* crack(const int length) {
+char* crack(const int length, const char start, const char end) {
 	char* guess = (char*) malloc(length + 1);
 	guess[length] = '\0';
-	return crackHelper(guess, 0, length);
+	return crackHelper(guess, 0, length, start, end);
 }
 
 
 int main(int argc, char** argv) {
 
-	if (argc != 4) { usage(); }
+	if (argc < 4 || 5 < argc) { usage(); }
 	long int threads = strtol(argv[1], NULL, 10);
 	long int keysize = strtol(argv[2], NULL, 10);
 	target = argv[3];
 	strncpy(salt, target, 2);
 
+	char start = 'a';
+	char end = 'z';
+
+	if (argc == 5) {
+		long int expanded = strtol(argv[4], NULL, 10);
+		if (expanded) {
+			start = ' ';
+			end = '~';
+		}
+	}
+
+	//int threadResponsibility[threads];
+	//for (int i = 0; i < threads, i++) {
+	//	threadResponsibility[i] = keysize/threads;
+	//}
+	//for (int i = 0; i < keysize % threads; i++) {
+	//	threadResponsibility[i] = threadResponsibility[i] + 1;
+	//}
+	
 	for (int length = 1; length <= keysize; length++) {
-		char* result = crack(length);
+		char* result = crack(length, start, end);
 		if (result) {
 			printf("%s\n", result);
 			break;
